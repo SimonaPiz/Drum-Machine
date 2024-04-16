e046f4d7a4659c3d1019bc36077c93344c0365df
Simona Pizio <simona.pizio@gmail.com>
2024-04-16 11:26:02 +0200

    Create react app to init the project

---
+++ drum-machine/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        Drum Machine 
+        <i class="fa-solid fa-drum icon"></i>
+      </h1>
+    </div>
+  );
+}
+
+export default App;

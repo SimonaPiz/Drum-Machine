f8571522b34ff11f0cbbec28e9065612ed3a6c09
Simona Pizio <simona.pizio@gmail.com>
2024-04-16 13:03:07 +0200

    create react elements #1 - add main component

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,10 +1,14 @@
+import { Display } from "./Components/Display/Display";
+
 function App() {
   return (
-    <div className="App">
+    <div id="drum-machine">
       <h1>
         Drum Machine 
         <i class="fa-solid fa-drum icon"></i>
       </h1>
+      <div id="pads-container"></div>
+      <Display />
     </div>
   );
 }

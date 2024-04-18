b1e8cf7dd8abf131741473d0d1bf231ba7f60eca
Simona Pizio <simona.pizio@gmail.com>
2024-04-18 16:31:26 +0200

    create react elements #1 - add drums file

---
+++ drum-machine/src/drums.js
@@ -0,0 +1,47 @@
+export const drums = [
+  {
+    name: 'Heater 1',
+    char: 'q',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3'
+  },
+  {
+    name: 'Heater 2',
+    char: 'w',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3'
+  },
+  {
+    name: 'Heater 3',
+    char: 'e',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3'
+  },
+  {
+    name: 'Heater 4',
+    char: 'a',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3'
+  },
+  {
+    name: 'Clap',
+    char: 's',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3'
+  },
+  {
+    name: 'Open HH',
+    char: 'd',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3'
+  },
+  {
+    name: 'Kick n\' Hat',
+    char: 'z',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3'
+  },
+  {
+    name: 'Kick',
+    char: 'x',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3'
+  },
+  {
+    name: 'Closed HH',
+    char: 'c',
+    url: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3'
+  }
+];

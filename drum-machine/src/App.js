0876f5a4fa9c9d13f297f625a99caffe942c6169
Simona Pizio <simona.pizio@gmail.com>
2024-04-19 11:58:54 +0200

    Menage status - #5 #4 - Connect store to app

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -1,8 +1,14 @@
 import { drums } from "./drums";
 import { DrumMachine } from "./features/DrumMachine/DrumMachine";
+import { Provider } from "react-redux";
+import { store } from './store';
 
 function App() {
-  return <DrumMachine drums={drums} />;
+  return (
+    <Provider store={store} >
+      <DrumMachine drums={drums} />
+    </Provider>
+  );
 }
 
 export default App;
